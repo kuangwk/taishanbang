5820031a656b791fb1b46b9132bcf565e8383749
kuangwk <kuangweike@gmail.com>
2014-05-15 01:35:50 +0800

    add slide

---
+++ slide/js/render.js
@@ -0,0 +1,55 @@
+;(function(){
+
+  // sec1 
+  $('#sec1-avatar').css({backgroundImage: "url('./img/" + sec1.avatar + "')"})
+  $('#sec1-title').css({backgroundImage: "url('./img/" + sec1.title + "')"})
+
+  // sec2
+  $('#sec2-title1').text(sec2.title1)
+  $('#sec2-title2').text(sec2.title2)
+  $('#sec2-title3').text(sec2.title3)
+  $('#sec2-text1').text(sec2.text1)
+  $('#sec2-text2').text(sec2.text2)
+  $('#sec2-text3').text(sec2.text3)
+
+  // sec3
+  $('#sec3-title1').text(sec3.title1)
+  $('#sec3-title2').text(sec3.title2)
+  $('#sec3-title3').text(sec3.title3)
+  $('#sec3-text1').text(sec3.text1)
+  $('#sec3-text2').text(sec3.text2)
+  $('#sec3-text3').text(sec3.text3)
+
+  // sec4
+  $('#sec4-title1').text(sec4.title1)
+  $('#sec4-title2').text(sec4.title2)
+  $('#sec4-title3').text(sec4.title3)
+  $('#sec4-text1').text(sec4.text1)
+  $('#sec4-text2').text(sec4.text2)
+  $('#sec4-text3').text(sec4.text3)
+
+  // sec5
+  $('#sec5-title1').text(sec5.title1)
+  $('#sec5-title2').text(sec5.title2)
+  $('#sec5-title3').text(sec5.title3)
+  $('#sec5-text1').text(sec5.text1)
+  $('#sec5-text2').text(sec5.text2)
+  $('#sec5-text3').text(sec5.text3)
+
+  //sec6
+  $('#sec6-title1').text(sec6.title1)
+  $('#sec6-title2').text(sec6.title2)
+  $('#sec6-image1').css({backgroundImage: "url('./img/" + sec6.image1 + "')"})
+
+  // sec7
+  // $('#cov7').css('display', 'none')
+  // $('#cov8').css('display', 'none')
+  // $('#cov9').css('display', 'none')
+  // $('#cov10').css('display', 'none')
+  // $('#sec7').css('display', 'none')
+  // $('#sec8').css('display', 'none')
+  // $('#sec9').css('display', 'none')
+  // $('#sec10').css('display', 'none')
+
+
+}());
